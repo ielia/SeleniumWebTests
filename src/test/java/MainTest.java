38b7e670b4f82625753f8b08b232562586202bf2
Ignacio Elia <elia.ignacio@gmail.com>
2020-10-13 20:47:23 -0300

    Microsoft y la puta madre que te remil parió. Hijo de un MOL de putas.

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,100 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainTest {
+    private static final Logger log = LoggerFactory.getLogger(MainTest.class);
+    protected static final Map<String, List<MessagePair>> testMessages = new LinkedHashMap<>();
+    protected WebDriver driver;
+    protected long testStartTime;
+    protected TestInfo testInfo;
+
+    static Stream<Arguments> localParameters() {
+        return Stream.of(
+            Arguments.of("http://www.microsoft.com/"),
+            Arguments.of("https://www.google.com/")
+        );
+    }
+
+    public MainTest() {}
+
+    protected void logTime(String category, long startTime) {
+        long timespan = new Date().getTime() - startTime;
+        String message = category + " Time: " + timespan + "ms";
+        String key = testInfo.getTestMethod().get().getName() + " - " + category;
+        String arguments = testInfo.getDisplayName().replaceFirst("^\\[\\d+\\] => ", "");
+        testMessages.computeIfAbsent(key, k -> new LinkedList<>()).add(new MessagePair(arguments, message, timespan));
+        log.info(message);
+    }
+
+    @BeforeEach
+    public void beforeEach(TestInfo testInfo) {
+        this.testInfo = testInfo;
+        long start = new Date().getTime();
+        WebDriverManager.chromedriver().setup();
+        logTime("PREPARATION", start);
+        start = new Date().getTime();
+        driver = new ChromeDriver();
+        logTime("DRIVER CREATION", start);
+        testStartTime = new Date().getTime();
+    }
+
+    @ParameterizedTest(name = "[{index}] => {0}")
+    @MethodSource("localParameters")
+    public void test1(String url) {
+        long start = new Date().getTime();
+        driver.get("data:,");
+        //log.info("TITLE: {}", driver.getTitle());
+        logTime("WINDOW OPEN", start);
+        start = new Date().getTime();
+        driver.get(url);
+        logTime("URL OPEN", start);
+    }
+
+    @ParameterizedTest(name = "[{index}] => {0}")
+    @MethodSource("localParameters")
+    public void test2(String url) {
+        long start = new Date().getTime();
+        driver.get("data:,");
+        //log.info("TITLE: {}", driver.getTitle());
+        logTime("MY WINDOW OPEN", start);
+        start = new Date().getTime();
+        driver.get(url);
+        logTime("MY URL OPEN", start);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        logTime("TEST RUN", testStartTime);
+        long start = new Date().getTime();
+        driver.quit();
+        logTime("DRIVER QUIT", start);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        log.info("CSV: operation name, {}", localParameters().map(e -> "time in ms for " + e.get()[0]).collect(Collectors.joining(", ")));
+        for (Map.Entry<String, List<MessagePair>> entry : testMessages.entrySet()) {
+            //log.info("CSV: {}, {}", entry.getKey(), entry.getValue().stream().map(e -> "(" + e.arguments + ") " + e.timespan).collect(Collectors.joining(", ")));
+            log.info("CSV: {}, {}", entry.getKey(), entry.getValue().stream().map(e -> "" + e.timespan).collect(Collectors.joining(", ")));
+        }
+    }
+}
---
+++ src/test/java/MessagePair.java
@@ -0,0 +1,11 @@
+public class MessagePair {
+    public final String arguments;
+    public final String message;
+    public final long timespan;
+
+    public MessagePair(String arguments, String message, long timespan) {
+        this.arguments = arguments;
+        this.message = message;
+        this.timespan = timespan;
+    }
+}
